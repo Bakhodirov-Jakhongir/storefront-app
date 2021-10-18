024d3cfceb804883e0fa2d971b9cd8221276bfc4
Bakhodirov-Jakhongir <bahodirovjahongirxoja@gmail.com>
2021-10-18 17:06:58 +0500

    installed-django-rest-framework and created-view

--- store/views.py
+++ store/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.request import Request
 
 # Create your views here.
+
+#all products
+@api_view()
+def product_list(request):
+    return Response('ok')
+    
+#retrieve single product
+@api_view()
+def product_detail(request,id):
+    return Response(id)
