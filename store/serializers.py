ca4e5f0695b3d3b9a812afa8d4c0fd6e5f04fec5
Bakhodirov-Jakhongir <bahodirovjahongirxoja@gmail.com>
2021-10-20 16:46:47 +0500

    model serializers

--- store/serializers.py
+++ store/serializers.py
@@ -2,18 +2,24 @@ from rest_framework import serializers
 from . models import Collection, Product
 from decimal import Decimal
 
-class CollectionSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
-class ProductSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
-    price = serializers.DecimalField(max_digits=6 , decimal_places=2 , source='unit_price')
+class CollectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Collection
+        fields = ['id' , 'title' , 'featured_product'] 
+    # id = serializers.IntegerField()
+    # title = serializers.CharField(max_length=255)
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id' , 'title' , 'unit_price' , 'price_with_tax' , 'collection']
+    # id = serializers.IntegerField()
+    # title = serializers.CharField(max_length=255)
+    # price = serializers.DecimalField(max_digits=6 , decimal_places=2 , source='unit_price')
     price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
-    collection = serializers.HyperlinkedRelatedField(
-        queryset = Collection.objects.all(),
-        view_name='collection-detail'
-    )
+    # collection = serializers.HyperlinkedRelatedField(
+    #     queryset = Collection.objects.all(),
+    #     view_name='collection-detail'
+    # )
 
     # collection = CollectionSerializer()  #call collection-serializer
 
